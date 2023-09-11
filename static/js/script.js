614ca58898839a3ccb9c66dc8e4423aafac112a1
Ellie Jones <ellie@ramseysystems.co.uk>
2023-09-11 13:53:15 +0100

    Story History Fixing - html_data broken still

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,11 @@
 //VARIABLE PREDEFINED
   let story_data = [];
   let storySave = 1;
+  let storyNextId = 1;
 
   let timeline_data = [];
   let timelineSave = 1;
-
-  let nextId = 1;
-  let currentRowId = null;
+  
 
 
 //////////////////////////NAVBAR//////////////////////////
@@ -29,65 +28,7 @@ function openPage(pageName,elmnt,color) {
   document.getElementById("defaultOpen").click();
 
 
-
-// 
-
-function updateStoryJSON() {
-
-  const rows = document.querySelectorAll("#tableBody tr");
-  rows.forEach(row => {
-      const id = parseInt(row.id.replace("row-", ""), 10);
-      const summaryCell = row.querySelector("[data-key='summary']");
-      const rationaleCell = row.querySelector("[data-key='rationale']");
-      const storyCell = row.querySelector("[data-key='story']");
-
-      // Find the story_data entry by id or create a new one if it doesn't exist
-      let dataObj = story_data.find(x => x.id === id);
-      if (!dataObj) {
-          dataObj = { id: id };
-          story_data.push(dataObj);
-      }
-
-      dataObj.Summary = summaryCell.textContent;
-      dataObj.Rationale = rationaleCell.textContent;
-      dataObj.Story = storyCell.textContent;
-  });
-  console.log(story_data);
-  updateJSONDisplay();
-}
-
-document.getElementById("addStoryRow").addEventListener("click", function () {
-  const newRow = `
-      <tr id="row-${nextId}">
-          <td data-key="summary" contenteditable="true" oninput="updateStoryJSON()"></td>
-          <td data-key="rationale" contenteditable="true" oninput="updateStoryJSON()"></td>
-          <td data-key="story" contenteditable="true" oninput="updateStoryJSON()"></td>
-          <td>
-              <button class="btn btn-danger" onclick="deleteRow(${nextId})">Delete</button>
-          </td>
-      </tr>
-  `;
-
-  document.getElementById("tableBody").innerHTML += newRow;
-  nextId++;
-  updateStoryJSON();
-  updateJSONDisplay();
-});
-
-function deleteRow(id) {
-  const row = document.getElementById("row-" + id);
-  row.parentNode.removeChild(row);
-
-  // Remove the object with the corresponding id from the story_data array
-  const index = story_data.findIndex(x => x.id === id);
-  if (index !== -1) {
-      story_data.splice(index, 1);
-  }
-
-  updateStoryJSON();
-  updateJSONDisplay();
-}
-
+///////////////////////////STORY//////////////////////////
 function updateJSONDisplay() {
   // If you have an element to show the JSON, use this. Otherwise, you can remove this function.
   const jsonOutputElem = document.getElementById("storyTextArea");
@@ -95,3 +36,101 @@ function updateJSONDisplay() {
       jsonOutputElem.value = JSON.stringify(story_data, null, 2);
   }
 }
+
+document.getElementById("addStoryRow").addEventListener("click", function () {
+  const newRow = document.createElement('tr');
+  newRow.id = `row-${storyNextId}`;
+
+  ['summary', 'rationale', 'story'].forEach(key => {
+    const cell = newRow.insertCell(-1);
+    cell.setAttribute('data-key', key);
+    cell.contentEditable = 'true';
+  });
+
+  const actionCell = newRow.insertCell(-1);
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.className = 'btn btn-danger';
+  deleteButton.dataset.id = storyNextId;
+  actionCell.appendChild(deleteButton);
+
+  document.getElementById("tableBody").appendChild(newRow);
+
+  story_data.push({id: storyNextId, Summary: '', Rationale: '', Story: ''});
+  storyNextId++;
+  updateJSONDisplay();
+});
+
+document.getElementById("tableBody").addEventListener('input', function (e) {
+  if (e.target && e.target.matches("[data-key]")) {
+    const key = e.target.getAttribute('data-key');
+    const id = parseInt(e.target.parentElement.id.replace("row-", ""), 10);
+    const dataObj = story_data.find(x => x.id === id);
+    if (dataObj) {
+      dataObj[key.charAt(0).toUpperCase() + key.slice(1)] = e.target.textContent;
+    }
+    updateJSONDisplay();
+  }
+});
+
+document.getElementById("tableBody").addEventListener('click', function (e) {
+  if (e.target && e.target.matches(".btn-danger")) {
+    const id = parseInt(e.target.dataset.id, 10);
+    e.target.parentElement.parentElement.remove();
+
+    const index = story_data.findIndex(x => x.id === id);
+    if (index !== -1) {
+      story_data.splice(index, 1);
+    }
+    updateJSONDisplay();
+  }
+});
+
+
+$('#story-history-button').click(function() {
+  let story_data = [
+    {
+      "id": 1,
+      "Summary": "",
+      "Rationale": "",
+      "Story": ""
+    }
+  ];
+
+  let headingVar = "storyHeading" + storySave;
+  let collapseVar = "storyCollapse" + storySave;
+
+  let story_data_json = story_data;
+  
+  let html_data = '';
+
+  // for (let i = 0; i < story_data_json.length; i++) {
+  //   let formattedStoryData = '';
+  //   for (let key in story_data_json[i]) {
+  //     formattedStoryData += `<strong>${key}:</strong> ${story_data_json[i][key]}<br/>`;
+  //   }
+
+  //   html_data += `
+  //     <div class="accordion-item">
+  //       <h2 class="accordion-header" id="${headingVar + i}">
+  //         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${collapseVar + i}" aria-expanded="false" aria-controls="${collapseVar + i}">
+  //           Accordion Item #${storySave + i}
+  //         </button>
+  //       </h2>
+  //       <div id="${collapseVar + i}" class="accordion-collapse collapse" aria-labelledby="${headingVar + i}" data-bs-parent="#accordionExample">
+  //         <div class="accordion-body">
+  //           ${formattedStoryData}
+  //         </div>
+  //       </div>
+  //     </div>
+  //   `;
+  //  }
+
+  html_data += `<p>Story</p>`
+
+  $('#storyHistoryContent').append(html_data);
+
+  storySave = storySave + 1;
+
+ });
+
